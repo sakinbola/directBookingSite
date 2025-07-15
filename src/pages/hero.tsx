119120c3d31f7f1a864c6673ad97fc56be459bd7
Seyi Akinbola <sakinbolaa@gmail.com>
2025-07-15 08:11:21 -0400

    img imports changed

--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -1,180 +1,91 @@
 import "./hero.css"
 
+// Import images statically
+import img1 from "./../images/img_1.jpg";
+import img2 from "./../images/img_2.jpg";
+import img3 from "./../images/img_3.jpg";
+import img4 from "./../images/img_4.jpg";
+import img5 from "./../images/img_5.jpg";
+import img6 from "./../images/img_6.jpg";
+import img7 from "./../images/img_7.jpg";
+import img8 from "./../images/img_8.jpg";
+import img9 from "./../images/img_9.jpg";
+import img10 from "./../images/img_10.jpg";
+import img11 from "./../images/img_11.jpg";
+import img12 from "./../images/img_12.jpg";
+import img13 from "./../images/img_13.jpg";
+import img14 from "./../images/img_14.jpg";
+import img15 from "./../images/img_15.jpg";
+import img16 from "./../images/img_16.jpg";
+import img17 from "./../images/img_17.jpg";
+import img18 from "./../images/img_18.jpg";
+
 const heroData = {
     options: [ 
-        {
-            title: "Front Hall", 
-            source: "./../images/img_1.jpg"
-        }, 
-
-                {
-            title: "First Floor Bedroom", 
-            source: "./../images/img_2.jpg"
-        },
-                {
-            title: "Living Room", 
-            source: "./../images/img_3.jpg"
-        },
-                {
-            title: "Living Room 2", 
-            source: "./../images/img_4.jpg"
-        },
-                {
-            title: "Living Room 3", 
-            source: "./../images/img_5.jpg"
-        },
-                {
-            title: "Kitchen 1", 
-            source: "./../images/img_6.jpg"
-        },
-                {
-            title: "Kitchen 2", 
-            source: "./../images/img_7.jpg"
-        },
-                {
-            title: "Staircase 1", 
-            source: "./../images/img_8.jpg"
-        },
-                {
-            title: "Upstairs", 
-            source: "./../images/img_9.jpg"
-        },
-                {
-            title: "Laundry Room", 
-            source: "./../images/img_10.jpg"
-        },
-                {
-            title: "Upstairs Bathroom", 
-            source: "./../images/img_11.jpg"
-        },
-                {
-            title: "Upstairs Bedroom", 
-            source: "./../images/img_12.jpg"
-        },
-                {
-            title: "Upstaris Bedroom 2", 
-            source: "./../images/img_13.jpg"
-        },
-                {
-            title: "Staircase 2", 
-            source: "./../images/img_14.jpg"
-        },
-                {
-            title: "Master Bedroom", 
-            source: "./../images/img_15.jpg"
-        },
-                        {
-            title: "Master Washroom", 
-            source: "./../images/img_16.jpg"
-        },
-                        {
-            title: "Master Bath Tub", 
-            source: "./../images/img_17.jpg"
-        },
-                        {
-            title: "Front Door", 
-            source: "./../images/img_18.jpg"
-        },
-
-
+        { title: "Front Hall", source: img1 },
+        { title: "First Floor Bedroom", source: img2 },
+        { title: "Living Room", source: img3 },
+        { title: "Living Room 2", source: img4 },
+        { title: "Living Room 3", source: img5 },
+        { title: "Kitchen 1", source: img6 },
+        { title: "Kitchen 2", source: img7 },
+        { title: "Staircase 1", source: img8 },
+        { title: "Upstairs", source: img9 },
+        { title: "Laundry Room", source: img10 },
+        { title: "Upstairs Bathroom", source: img11 },
+        { title: "Upstairs Bedroom", source: img12 },
+        { title: "Upstaris Bedroom 2", source: img13 },
+        { title: "Staircase 2", source: img14 },
+        { title: "Master Bedroom", source: img15 },
+        { title: "Master Washroom", source: img16 },
+        { title: "Master Bath Tub", source: img17 },
+        { title: "Front Door", source: img18 },
     ]
 }
-
-
-
 
 function Hero () {
     return (
         <div className="hero-section">
-
-
-
             <div className="property-slideshow">
-                {heroData.options.map(( {title,source} , index)  => (
+                {heroData.options.map(({ title, source }, index) => (
                     <PropertyPictures key={`Image ${index+1}`} title={title} source={source}/>
-
                 ))}
             </div>
-
             <div className="title">
                 <h1> Elegant Mississuaga Home </h1>   
             </div>  
-            
-            {/* <div className="image-container">
-
-                <img src={image1} alt="Image1" className="image-child" />
-
-                {/* <div className="design-container" >
-                    <p> Your Property </p>
-                </div> 
-
-                <img src={image2} alt="Image2" className="image-child" />
-
-
-            </div> */}
-
-            
             <div className="property-description">  
                 <p>
                     
                 </p>
             </div>
-
             <div className="property-details"> 
-
-
                 <h2> Property Details</h2>
-
                 <div className="details-container">
                     <h3> X </h3>
-
                     <p> x</p>
                 </div>
-
                 <div className="details-container">
                     <h3> X </h3>
-
                     <p> x</p>
                 </div>
-
-
                 <div className="details-container">
                     <h3> X </h3>
- 
                     <p> x</p>
                 </div>
-
-
             </div>
-
-
-
-
-
-
-
-
-
-
-
         </div>
     )
 }
 
 export default Hero 
 
-
 type PropertyCard = {
     title:string;
     source:string;
 };
 
-// // {footerData.options.map( ({title,link}) => (
-// // <OptionLinks key={title} title={title} link={link} />
-// // ))}
-
 const PropertyPictures = ({title,source}:PropertyCard) => {
-    
     return (
         <div className="slide" >
             <img src={source} alt={title} />
